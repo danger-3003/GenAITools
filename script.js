19b4eb9e42e05ce40841b5967322bb72db000238
danger-3003 <naremsumanth@gmail.com>
2023-10-24 16:23:10 +0530

    added toggle menu

---
+++ script.js
@@ -0,0 +1,20 @@
+var menu=document.getElementById("menu");
+var button=document.getElementById("bars");
+var nav=document.querySelector(".nav");
+button.addEventListener("click",()=>
+{
+    if(button.classList.contains("fa-bars"))
+    {
+        button.classList.replace("fa-bars","fa-times");
+        menu.classList.replace("h-0","h-[10.5rem]");
+        menu.classList.replace("-top-24","top-0");
+        menu.classList.remove("opacity-0");
+    }
+    else
+    {
+        button.classList.replace("fa-times","fa-bars");
+        menu.classList.replace("h-[10.5rem]","h-0");
+        menu.classList.replace("top-0","-top-24");
+        menu.classList.add("opacity-0");
+    }
+})
