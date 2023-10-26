f68e03d1390a883f25a7338194daed13c0f8c442
danger-3003 <naremsumanth@gmail.com>
2023-10-26 15:04:44 +0530

    updated colors

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ button.addEventListener("click",()=>
     {
         button.classList.replace("fa-bars","fa-times");
         menu.classList.replace("h-0","h-[10.5rem]");
-        menu.classList.replace("-top-24","top-0");
+        menu.classList.replace("-top-28","top-0");
         menu.classList.remove("opacity-0");
     }
     else
     {
         button.classList.replace("fa-times","fa-bars");
         menu.classList.replace("h-[10.5rem]","h-0");
-        menu.classList.replace("top-0","-top-24");
+        menu.classList.replace("top-0","-top-28");
         menu.classList.add("opacity-0");
     }
 })
